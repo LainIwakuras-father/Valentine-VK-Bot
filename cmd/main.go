b9924cd8af278e3cdecde05ed1de78129fc3459b
Uriy Dolewsky <dol3wsky@gmail.com>
2026-02-05 05:46:53 +0300

    нашел библиотеку vksdk

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	// 	"go/token"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/SevereCloud/vksdk/v3/api"
+
+	"github.com/SevereCloud/vksdk/v3/events"
+	longpoll "github.com/SevereCloud/vksdk/v3/longpoll-bot"
 )
 
-// сука ебанная заработала я хуею
 func main() {
-	fmt.Println("Hello")
+	// НЕ ЗАБЫВАЙ ПРО ПЕРЕМЕННЫЕ ОКРУЖЕНИЯ
+
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Ошибка: .env file not found: %v", err)
+	}
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatal("Переменная окружения TOKEN не установлена!")
+	}
+	vk := api.NewVK(token)
+
+	lp, err := longpoll.NewLongPoll(vk, 235791902)
+	if err != nil {
+		log.Fatal("Ошибка инициализации LongPoll:", err)
+		panic(err)
+	}
+
+	lp.MessageNew(func(ctx context.Context, obj events.MessageNewObject) {
+		log.Print(obj.Message.Text)
+	})
+
+	lp.Run()
 }
