5d0dc24c7fd62178deb99f27bcd2db74c3c3b09b
Uriy Dolewsky <dol3wsky@gmail.com>
2026-02-05 05:57:50 +0300

    теперь с ботом можно взаимодействовать

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func main() {
 		log.Fatal("Переменная окружения TOKEN не установлена!")
 	}
 	vk := api.NewVK(token)
-
+	log.Printf("Иницилизируем бота...")
 	lp, err := longpoll.NewLongPoll(vk, 235791902)
 	if err != nil {
 		log.Fatal("Ошибка инициализации LongPoll:", err)
@@ -36,5 +36,17 @@ func main() {
 		log.Print(obj.Message.Text)
 	})
 
-	lp.Run()
+	log.Printf("Запускаем бота...")
+	// Запуск
+	if err := lp.Run(); err != nil {
+		log.Fatal("Бот не смог запустится", err)
+	}
+
+	// Безопасное завершение
+	// Ждет пока соединение закроется и события обработаются
+	lp.Shutdown()
+
+	// Закрыть соединение
+	// Требует lp.Client.Transport = &http.Transport{DisableKeepAlives: true}
+	lp.Client.CloseIdleConnections()
 }
